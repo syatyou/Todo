b61d617dc579a85103cde6314d19f5a6ec6c4097
itacademy <itacademy@192.168.1.2>
2013-03-16 21:32:22 +0900

    owari

---
+++ test/todo/test/TestUtil.java
@@ -0,0 +1,18 @@
+package todo.test;
+
+import java.util.Date;
+
+import todo.model.Todo;
+
+public class TestUtil {
+    
+    public static Todo newTodo(String userId, String body, boolean finished) {
+        Todo todo = new Todo();
+        todo.setUserId(userId);
+        todo.setBody(body);
+        todo.setCreatedAt(new Date());
+        todo.setFinished(finished);
+        todo.setFinishedAt(finished ? new Date() : null);
+        return todo;
+    }
+}
